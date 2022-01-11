7120919c16a28110cd7ba48bd4f768523123dcff
GR Silva <grscookie@gmail.com>
2022-01-11 09:02:43 -0300

    calculadora

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿{
+    Console.Write("Professor, digite a primeira nota: ");
+    int n1 = int.Parse(Console.ReadLine());
+
+    Console.Write("Professor, digite a segunda nota: ");
+    int n2 = int.Parse(Console.ReadLine());
+
+    Console.Write("Professor, digite a terceira nota: ");
+    int n3 = int.Parse(Console.ReadLine());
+
+    Console.Write("Professor, digite a última nota: ");
+    int n4 = int.Parse(Console.ReadLine());
+
+    Console.Clear();
+
+    Console.WriteLine("Aguarde enquanto o resultado é calculado...");
+
+    double med = (n1 + n2 + n3 + n4) / 4;
+
+    if (med < 6)
+    {
+        Console.WriteLine("A nota final é igual a " + med);
+        Console.WriteLine("O aluno está de recuperação.");
+    }
+
+    else
+    {
+
+        Console.WriteLine("A nota final é igual a " + med);
+        Console.WriteLine("O aluno está aprovado. Parabéns.");
+    }
+
+
+}
